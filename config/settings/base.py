a484bb0bf7d1ff9cc618170e712f902f4c9af923
go-arnold <arnoldciku@gmail.com>
2026-03-04 23:26:51 +0200

    fix allwed hosts

--- config/settings/base.py
+++ config/settings/base.py
@@ -10,10 +10,9 @@ environ.Env.read_env(BASE_DIR / ".env")
 
 SECRET_KEY = env("SECRET_KEY")
 DEBUG = env.bool("DEBUG", default=False)
-ALLOWED_HOSTS = env.list(
+ALLOWED_HOSTS = env(
     "ALLOWED_HOSTS",
     default=[
-        "unpointing-repercussively-annamae.ngrok-free.dev",
         "localhost",
         "127.0.0.1",
         "https://senior-madeleine-matabar-93648cd5.koyeb.app/",
@@ -193,6 +192,6 @@ CORS_ALLOWED_ORIGINS = env.list(
 CSRF_TRUSTED_ORIGINS = env.list(
     "CSRF_TRUSTED_ORIGINS",
     default=[
-        "hhttps://senior-madeleine-matabar-93648cd5.koyeb.app/",
+        "https://senior-madeleine-matabar-93648cd5.koyeb.app/",
     ],
 )
