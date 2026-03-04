ca07791508ac07c4698b3ce30ce6541cf1cc839d
go-arnold <arnoldciku@gmail.com>
2026-03-04 23:45:03 +0200

    main url -- home

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 
+from apps.dashboard.spa_view import DashboardAppView
+
 urlpatterns = [
+    path("", DashboardAppView.as_view(), name="app"),
     path("admin/", admin.site.urls),
     path("api/webhook/", include("apps.webhook.urls")),
     path("api/dashboard/", include("apps.dashboard.urls")),
