76bb6043e4c4a875a3dcbef5a4b79e56122a8963
ArmandCito <armandcito@gmail.com>
2026-03-16 22:53:31 +0200

    CRsf errors resolved

--- config/settings/base.py
+++ config/settings/base.py
@@ -189,13 +189,14 @@ CELERY_BEAT_SCHEDULE = {
 #     "CORS_ALLOWED_ORIGINS",
 #     default=["https://senior-madeleine-matabar-93648cd5.koyeb.app/"],
 # )
-CORS_ALLOWED_ORIGINS = env("CORS_ALLOWED_ORIGINS")
+CORS_ALLOWED_ORIGINS ="https://senior-madeleine-matabar-93648cd5.koyeb.app/"
 # CSRF_TRUSTED_ORIGINS = env.list(
 #     "CSRF_TRUSTED_ORIGINS", 
 #     default=[
 #         "https://senior-madeleine-matabar-93648cd5.koyeb.app/",
 #     ],
 # )
-CSRF_TRUSTED_ORIGINS = env("CSRF_TRUSTED_ORIGINS")
+CSRF_TRUSTED_ORIGINS ="https://senior-madeleine-matabar-93648cd5.koyeb.app/"
 
+    
 # APPEND_SLASH = False
